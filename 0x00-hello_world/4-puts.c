5456f6531880b42b0e7649a2ce7c2015e179f075
Yeabsira27 <alemuy40@gmail.com>
2022-10-16 05:19:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+   int main(void)
+{
+	puta("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
